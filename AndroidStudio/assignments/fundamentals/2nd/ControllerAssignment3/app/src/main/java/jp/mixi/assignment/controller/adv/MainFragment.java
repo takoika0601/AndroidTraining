cb0cd63cfc0f05fc8f6c77c3f9d460b45fc1362b
takoika0601 <takoika0601@akiguchi-no-MacBook-Pro.local>
2014-06-26 17:37:22 +0900

    assignment3 finished

---
+++ AndroidStudio/assignments/fundamentals/2nd/ControllerAssignment3/app/src/main/java/jp/mixi/assignment/controller/adv/MainFragment.java
@@ -0,0 +1,33 @@
+package jp.mixi.assignment.controller.adv;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
