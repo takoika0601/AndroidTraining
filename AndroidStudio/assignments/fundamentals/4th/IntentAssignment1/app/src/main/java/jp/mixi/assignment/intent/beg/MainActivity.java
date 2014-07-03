24b581863551fbd32faa7cf80796893f70b4879e
takoika0601 <takoika0601@akiguchi-no-MacBook-Pro.local>
2014-07-03 14:01:40 +0900

    IntentAssignment1 finished

---
+++ AndroidStudio/assignments/fundamentals/4th/IntentAssignment1/app/src/main/java/jp/mixi/assignment/intent/beg/MainActivity.java
@@ -0,0 +1,27 @@
+
+package jp.mixi.assignment.intent.beg;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View button = findViewById(R.id.ViewMixi);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO http://mixi.jp をブラウザで開くための Intent を作って、ブラウザを立ち上げる
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://mixi.jp"));
+                startActivity(intent);
+
+            }
+        });
+    }
+}
