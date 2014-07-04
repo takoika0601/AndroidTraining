f8577fec4620aaaa69fc0401edc31f1e4f0f2361
takoika0601 <takoika0601@akiguchi-no-MacBook-Pro.local>
2014-07-04 17:24:20 +0900

    chapter_204 finished

---
+++ AndroidStudio/assignments/fundamentals/4th/NotificationAssignment2/app/src/main/java/jp/mixi/assignment/notification/med/MainActivity.java
@@ -0,0 +1,38 @@
+
+package jp.mixi.assignment.notification.med;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+public class MainActivity extends Activity {
+    public static final String ACITON_VIEW_MY_OWN = "jp.mixi.assignment.notification.med.android.intent.action.VIEW_MY_OWN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO ここで通知を表示する
+        // TODO intent には、ACTION_VIEW_MY_OWN の action をセットするだけにしておく
+        // TODO 通知をタップした時に、複数の選択肢が表示されるが、どれがどの Activity に対応しているかがわかりづらくなっているので、AndroidManifest を編集して表示をわかりやすくする
+        Intent intent = new Intent(ACITON_VIEW_MY_OWN);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Notification notification = builder
+                .setContentTitle("NotificationAssignment2")
+                .setContentText("Notification Message")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, notification);
+    }
+}
