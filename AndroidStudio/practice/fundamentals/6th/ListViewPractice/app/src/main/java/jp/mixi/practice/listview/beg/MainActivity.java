f88202f44ba33a2141ac3a21c3e804483c1c3286
takoika0601 <takoika0601@akiguchi-no-MacBook-Pro.local>
2014-07-08 13:12:39 +0900

    ListViewPractices finished

--- AndroidStudio/practice/fundamentals/6th/ListViewPractice/app/src/main/java/jp/mixi/practice/listview/beg/MainActivity.java
+++ AndroidStudio/practice/fundamentals/6th/ListViewPractice/app/src/main/java/jp/mixi/practice/listview/beg/MainActivity.java
@@ -4,6 +4,13 @@ package jp.mixi.practice.listview.beg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.view.View.OnClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +18,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> objects = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            objects.add("hoge" + i);
+        }
+
+        CustomListItemAdapter customListItemAdapter = new CustomListItemAdapter(this, objects);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(customListItemAdapter);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ListView) findViewById(R.id.listView)).smoothScrollToPosition(0);
+            }
+        });
     }
 
     @Override
@@ -19,5 +43,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-
 }
